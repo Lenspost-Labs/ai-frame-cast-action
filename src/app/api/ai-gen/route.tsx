dd9dd585fca4fdefb67e77d52570aba1988c574b
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2024-06-25 15:03:06 +0530

    Added timer for image response

--- src/app/api/ai-gen/route.tsx
+++ src/app/api/ai-gen/route.tsx
@@ -16,13 +16,176 @@ const fnQueueFalAPI = async (message: string) => {
   return response.json();
 };
 
+const fnGetStatusAPI = async (request_id: string) => {
+  const response = await fetch(
+    `https://queue.fal.run/fal-ai/fast-sdxl/requests/${request_id}/status`,
+    {
+      headers: {
+        Authorization: `Key ${FAL_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      method: 'GET'
+    }
+  );
+  return response.json();
+};
+
+const falGetImageAPI = async (response_url: string) => {
+  const response = await fetch(response_url, {
+    headers: {
+      Authorization: `Key ${FAL_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
+  });
+  return response.json();
+};
+
 // @ts-ignore
 const handler = frames(async (ctx) => {
+  const startTime = Date.now();
   const state = JSON.parse(ctx?.message?.state ? ctx?.message.state : '{}');
   const count = state.generateCount + 1;
   const prompt = ctx.message?.inputText as string;
   const res = await fnQueueFalAPI(prompt);
   const ImageID = res?.request_id;
+  while (Date.now() - startTime < 2800) {
+    const statusRes = await fnGetStatusAPI(ImageID);
+    const status = statusRes?.status;
+    if (status !== 'IN_QUEUE' && status !== 'IN_PROGRESS') {
+      const res_url = res?.response_url;
+      const response = await falGetImageAPI(res_url);
+      const state = JSON.parse(ctx.message?.state || '{}');
+      const count = state.generateCount;
+      const image_url = response.images[0].url;
+      if (count === 1) {
+        return {
+          buttons: [
+            <Button
+              target={`${APP_URL}/api/ai-gen`}
+              key="Generate1Button"
+              action="post"
+            >
+              Generate 1/5
+            </Button>,
+            <Button
+              target={`${APP_URL}/frames-hojayega/test/2`}
+              key="MintButton"
+              action="post"
+            >
+              Let&apos;s Mint
+            </Button>
+          ],
+          state: {
+            generateCount: count,
+            imageUrl: image_url
+          },
+          textInput: 'New Prompt',
+          image: image_url
+        };
+      } else if (count === 2) {
+        return {
+          buttons: [
+            <Button
+              target={`${APP_URL}/api/ai-gen`}
+              key="GenerateButton"
+              action="post"
+            >
+              Generate 2/5
+            </Button>,
+            <Button
+              target={`${APP_URL}/frames-hojayega/test/2`}
+              key="MintButton"
+              action="post"
+            >
+              Let&apos;s Mint
+            </Button>
+          ],
+          state: {
+            generateCount: count,
+            imageUrl: image_url
+          },
+          textInput: 'New Prompt',
+          image: image_url
+        };
+      } else if (count === 3) {
+        return {
+          buttons: [
+            <Button
+              target={`${APP_URL}/api/ai-gen`}
+              key="GenerateButton"
+              action="post"
+            >
+              Generate 3/5
+            </Button>,
+            <Button
+              target={`${APP_URL}/frames-hojayega/test/2`}
+              key="MintButton"
+              action="post"
+            >
+              Let&apos;s Mint
+            </Button>
+          ],
+          state: {
+            generateCount: count,
+            imageUrl: image_url
+          },
+          textInput: 'New Prompt',
+          image: image_url
+        };
+      } else if (count === 4) {
+        return {
+          buttons: [
+            <Button
+              target={`${APP_URL}/api/ai-gen`}
+              key="GenerateButton"
+              action="post"
+            >
+              Generate 4/5
+            </Button>,
+            <Button
+              target={`${APP_URL}/frames-hojayega/test/2`}
+              key="MintButton"
+              action="post"
+            >
+              Let&apos;s Mint
+            </Button>
+          ],
+          state: {
+            generateCount: count,
+            imageUrl: image_url
+          },
+          textInput: 'New Prompt',
+          image: image_url
+        };
+      } else if (count === 5) {
+        return {
+          buttons: [
+            <Button
+              target={`${APP_URL}/api/ai-gen`}
+              key="GenerateButton"
+              action="post"
+            >
+              Generate 5/5
+            </Button>,
+            <Button
+              target={`${APP_URL}/frames-hojayega/test/2`}
+              key="MintButton"
+              action="post"
+            >
+              Let&apos;s Mint
+            </Button>
+          ],
+          state: {
+            generateCount: count,
+            imageUrl: image_url
+          },
+          textInput: 'New Prompt',
+          image: image_url
+        };
+      } else return { image: <span>Generates Used Up!</span> };
+    }
+  }
   return {
     buttons: [
       <Button
